1e85a1782d1e022ecce30104227955080000bb65
Karna <kumarkarna2@yahoo.com>
2023-01-17 10:35:58 +0530

    added directed

--- matrix_directed.cpp
+++ matrix_directed.cpp
@@ -8,6 +8,30 @@ int main()
          << "\n";
     cin >> ver;
 
-    vector<pair<int, int>> adj[ver];
+    int adj[ver][ver] = {0};
+    for (int i = 0; i < ver; i++)
+    {
+        int a;
+        cout << "Enter number of outgoing edge from V" << i + 1 << " : "
+             << "\n";
+        cin >> a;
+        for (int j = 0; j < a; j++)
+        {
+            int b;
+            cout << "Enter vertex to which V" << i + 1 << " is connected to : ";
+            cin >> b;
+            adj[i][b - 1] = 1;
+        }
+    }
+    // printing the matrix
+    for (int i = 0; i < ver; i++)
+    {
+        for (int j = 0; j < ver; j++)
+        {
+            cout << adj[i][j] << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
